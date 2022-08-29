9e3d75e7da6bf9f28c651952b4df1dc931c98e27
Darius Duta <dduta065@gmail.com>
2022-08-29 22:42:57 +0100

    organise mobile navbar code

--- assets/js/mobile-navbar.js
+++ assets/js/mobile-navbar.js
@@ -1,14 +1,29 @@
+const NAVBAR_HEIGHT = 236; // px
+const TRANSITION_DURATION = 100; // ms
+
 toggleMobileNavbar = () => {
   const drawer = document.getElementById("mobile-navbar");
+  const isOpen = !drawer.classList.contains("h-0");
 
-  drawer.classList.toggle("h-0");
-  drawer.classList.toggle("border");
-  drawer.style.height = '238px';
+  if (!isOpen) {
+    drawer.classList.toggle("h-0");
+    drawer.classList.toggle("border");
+    drawer.style.height = `${NAVBAR_HEIGHT}px`;
 
-  setTimeout(() => {
-    drawer.style.height = null;
-    drawer.classList.toggle("h-fit");
-  }, 100);
+    setTimeout(() => {
+      drawer.classList.toggle("h-fit");
+      drawer.style.height = null;
+    }, TRANSITION_DURATION);
+  } else {
+    drawer.classList.toggle("h-0");
+    drawer.classList.toggle("border");
+    drawer.style.height = `${NAVBAR_HEIGHT}px`;
+
+    setTimeout(() => {
+      drawer.classList.toggle("h-fit");
+      drawer.style.height = null;
+    }, TRANSITION_DURATION);
+  }
 };
 
 window.addEventListener("DOMContentLoaded", () => {
